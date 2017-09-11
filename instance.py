bf396920e83041de708b791616183ffc0385d55d
indrah <indrhrph@gmail.com>
2017-09-11 19:42:00 -0400

    t

--- instance.py
+++ instance.py
@@ -1,5 +1,16 @@
-import boto3
+import boto3,sys
 
 ec2 = boto3.client('ec2')
 response = ec2.describe_instances()
+print(response)
+
+import sys
+
+
+
+ec2 = boto3.client('ec2')
+if sys.argv[1] == 'ON':
+    response = ec2.monitor_instances(InstanceIds=['INSTANCE_ID'])
+else:
+    response = ec2.unmonitor_instances(InstanceIds=['INSTANCE_ID'])
 print(response)
