fd5642cea7337bf96e6a35d045d44e416d1d2d09
root <root@puppetm.indra.com>
2017-09-21 14:00:14 +0000

    t

--- instance.py
+++ instance.py
@@ -1,16 +1,14 @@
+#!/bin/env python
 import boto3,sys
+INSTANCE_ID='i-0438130683c08f96b'
+ec2 = boto3.resource('ec2')
 
-ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
-print(response)
+#ec2 = boto3.client('ec2')
+#response = ec2.describe_instances()
+#print(response)
 
-import sys
-
-
-
-ec2 = boto3.client('ec2')
-if sys.argv[1] == 'ON':
-    response = ec2.monitor_instances(InstanceIds=['INSTANCE_ID'])
-else:
-    response = ec2.unmonitor_instances(InstanceIds=['INSTANCE_ID'])
-print(response)
+instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+for instance in instances:
+    print(instance.id, instance.instance_type)
+for status in ec2.meta.client.describe_instance_status()['InstanceStatuses']:
+    print(status)
